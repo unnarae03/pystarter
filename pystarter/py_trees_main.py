969cba7562268c4b973aad1cc59b65982c9b6bc8
강건하 <gunha0604@gmail.com>
2025-06-16 05:06:21 +0900

    r

--- pystarter/py_trees_main.py
+++ pystarter/py_trees_main.py
@@ -1,9 +1,8 @@
 import rclpy
 from rclpy.executors import MultiThreadedExecutor
-
 import py_trees
 
-from pystarter.nodes.move_to_goal_node import MoveToGoal, GoalSubscriber
+from pystarter.nodes.move_to_goal_node import MoveToGoal
 from pystarter.nodes.set_angle_node import SetAngle
 
 
@@ -20,9 +19,8 @@ def create_tree():
 def main():
     rclpy.init()
 
+    # ROS 2 노드 실행기 (멀티 스레드)
     executor = MultiThreadedExecutor()
-    goal_listener = GoalSubscriber()
-    executor.add_node(goal_listener)
 
     tree, move_to_goal_node, set_angle_node = create_tree()
     behaviour_tree = py_trees.trees.BehaviourTree(tree)
@@ -31,9 +29,6 @@ def main():
     try:
         while rclpy.ok():
             behaviour_tree.tick()
-
-            # 각 ROS2 노드 실행
-            rclpy.spin_once(goal_listener, timeout_sec=0.1)
             rclpy.spin_once(move_to_goal_node.node, timeout_sec=0.1)
             rclpy.spin_once(set_angle_node.node, timeout_sec=0.1)
 
@@ -41,7 +36,6 @@ def main():
         pass
 
     behaviour_tree.shutdown()
-    goal_listener.destroy_node()
     move_to_goal_node.node.destroy_node()
     set_angle_node.node.destroy_node()
     rclpy.shutdown()
